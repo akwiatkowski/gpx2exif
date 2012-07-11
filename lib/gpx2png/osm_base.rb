f07d3eea0a47961c8c82ae4516fa1b1e9cbca2f7
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-07-12 00:21:06 +0200

    autozoom

--- lib/gpx2png/osm_base.rb
+++ lib/gpx2png/osm_base.rb
@@ -94,23 +94,30 @@ module Gpx2png
       return { osm_title_coord: osm_tile_coord, pixel_offset: [x, y] }
     end
 
+    # Create image with fixed size
+    def fixed_size(_width, _height)
+      @fixed_width = _width
+      @fixed_height = _height
+    end
+
     def initial_calculations
       @lat_min = @coords.collect { |c| c[:lat] }.min
       @lat_max = @coords.collect { |c| c[:lat] }.max
       @lon_min = @coords.collect { |c| c[:lon] }.min
       @lon_max = @coords.collect { |c| c[:lon] }.max
 
-      # autozoom must be here
-      # drawing must fit into set resolution
-      # map must be bigger than set resolution
-      @best_full_image_x = 1000
-      @best_full_image_y = 1000
-      @new_zoom = self.class.calc_zoom(
-        @lat_min, @lat_max,
-        @lon_min, @lon_max,
-        @best_full_image_x, @best_full_image_y
-      )
-      puts @new_zoom
+      # auto zoom must be here
+      # drawing must fit into fixed resolution
+      # map must be bigger than fixed resolution
+      if @fixed_width and @fixed_height
+        @new_zoom = self.class.calc_zoom(
+          @lat_min, @lat_max,
+          @lon_min, @lon_max,
+          @fixed_width, @fixed_height
+        )
+        puts "Calculated new zoom #{@new_zoom} (was #{@zoom})" if @verbose
+        @zoom = @new_zoom
+      end
 
       @border_tiles = [
         self.class.convert(@zoom, [@lat_min, @lon_min]),
@@ -120,13 +127,29 @@ module Gpx2png
       @tile_x_range = (@border_tiles[0][0])..(@border_tiles[1][0])
       @tile_y_range = (@border_tiles[1][1])..(@border_tiles[0][1])
 
-      # new image
+      # enlarging ranges to fill up map area
+      # both sizes are enlarged
+      # = ( ( (preferred size - real size) / tile width ) / 2 ).ceil
+      if @fixed_width and @fixed_height
+        x_axis_expand_count = ((@best_full_image_x - (1 + @tile_x_range.max - @tile_x_range.min) * TILE_WIDTH).to_f / (TILE_WIDTH.to_f * 2.0)).ceil
+        y_axis_expand_count = ((@best_full_image_y - (1 + @tile_y_range.max - @tile_y_range.min) * TILE_HEIGHT).to_f / (TILE_HEIGHT.to_f * 2.0)).ceil
+        puts "Expanding X tiles from both sides #{x_axis_expand_count}" if @verbose
+        puts "Expanding Y tiles from both sides #{y_axis_expand_count}" if @verbose
+        @tile_x_range = ((@tile_x_range.min - x_axis_expand_count)..(@tile_x_range.max + x_axis_expand_count))
+        @tile_y_range = ((@tile_y_range.min - y_axis_expand_count)..(@tile_y_range.max + y_axis_expand_count))
+      end
+
+      # new/full image size
       @full_image_x = (1 + @tile_x_range.max - @tile_x_range.min) * TILE_WIDTH
       @full_image_y = (1 + @tile_y_range.max - @tile_y_range.min) * TILE_HEIGHT
       @r.x = @full_image_x
       @r.y = @full_image_y
 
-      calculate_for_crop
+      if @fixed_width and @fixed_height
+        calculate_for_crop_with_auto_zoom
+      else
+        calculate_for_crop
+      end
     end
 
     # Calculate zoom level
@@ -209,6 +232,18 @@ module Gpx2png
 
     # Calculate some numbers for cropping operation
     def calculate_for_crop
+      point_min = self.class.point_on_image(@zoom, [@lat_min, @lon_min])
+      point_max = self.class.point_on_image(@zoom, [@lat_max, @lon_max])
+      @bitmap_point_x_min = (point_min[:osm_title_coord][0] - @tile_x_range.min) * TILE_WIDTH + point_min[:pixel_offset][0]
+      @bitmap_point_x_max = (point_max[:osm_title_coord][0] - @tile_x_range.min) * TILE_WIDTH + point_max[:pixel_offset][0]
+      @bitmap_point_y_max = (point_min[:osm_title_coord][1] - @tile_y_range.min) * TILE_HEIGHT + point_min[:pixel_offset][1]
+      @bitmap_point_y_min = (point_max[:osm_title_coord][1] - @tile_y_range.min) * TILE_HEIGHT + point_max[:pixel_offset][1]
+
+      @r.set_crop(@bitmap_point_x_min, @bitmap_point_x_max, @bitmap_point_y_min, @bitmap_point_y_max)
+    end
+
+    # Calculate some numbers for cropping operation with autozoom
+    def calculate_for_crop_with_auto_zoom
       point_min = self.class.point_on_image(@zoom, [@lat_min, @lon_min])
       point_max = self.class.point_on_image(@zoom, [@lat_max, @lon_max])
       @bitmap_point_x_min = (point_min[:osm_title_coord][0] - @tile_x_range.min) * TILE_WIDTH + point_min[:pixel_offset][0]
