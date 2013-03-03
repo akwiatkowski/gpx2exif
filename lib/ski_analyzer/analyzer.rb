34160ca5e7296ee2aa01724d61519a116f81af84
Aleksander Kwiatkowski <bobikx@poczta.fm>
2013-03-03 22:40:05 +0100

    ski analyzer

--- lib/ski_analyzer/analyzer.rb
+++ lib/ski_analyzer/analyzer.rb
@@ -1,8 +1,14 @@
+require 'gpx_utils/waypoints_importer'
+
 module SkiAnalyzer
   class Analyzer
     def initialize(gpx_file)
       @file = gpx_file
-      puts @file
+
+      @i = GpxUtils::TrackImporter.new
+      @i.add_file(@file)
+
+      puts @i.coords.size
     end
   end
 end
--- spec/ski_analyzer/ski_analyzer_spec.rb
+++ spec/ski_analyzer/ski_analyzer_spec.rb
@@ -3,7 +3,7 @@ require 'ski_analyzer'
 
 describe SkiAnalyzer::Analyzer do
   it "should analyze GPX file created during ski activities" do
-    f = File.join('sample', 'ski.gpx')
+    f = File.join('spec', 'fixtures', 'ski.gpx')
     g = SkiAnalyzer::Analyzer.new(f)
   end
 end
