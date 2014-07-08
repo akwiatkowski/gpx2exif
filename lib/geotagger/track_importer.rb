026e50fb3e7828f0ecf7862e87a5d8b803af387b
Craig Taverner <craig@amanzi.com>
2014-07-08 14:31:51 +0200

    Fixed bug in processing GPX with fractional second accuracy

--- lib/geotagger/track_importer.rb
+++ lib/geotagger/track_importer.rb
@@ -14,6 +14,12 @@ module Geotagger
     attr_accessor :debug
     attr_accessor :direction_offset
 
+    def self.proc_time(ts)
+      if ts =~ /(\d{4})-(\d{2})-(\d{2})T(\d{1,2}):(\d{2}):([\d\.]{2,6})Z/
+        return Time.gm($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i, $6.to_f).localtime
+      end
+    end
+
     # Only import valid coords
     def self.coord_valid?(lat, lon, elevation, time)
       return true if lat and lon and time
